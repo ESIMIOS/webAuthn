cc7f864bf791e15196d80d65a7935e9e19de6875
tirsomartinezreyes <tirsomartinezreyes@gmail.com>
2023-01-16 11:07:17 -0600

    Construcción de la librería para distribución

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,29 +1,57 @@
-export type publicKeyCredentialCreationOptions = {
-    challenge: Uint8Array;
-    rp: {
-        name: string;
-        id: string;
+export type CredentialRegistrationData = {
+    id: string;
+    rawId: Uint8Array;
+    clienDataJSON: Uint8Array;
+    attestationObject: {
+        fmt: "packed";
+        attStmt: {
+            alg: number;
+            sig: Uint8Array;
+            x5c: Uint8Array[];
+        };
+        authData: {
+            rpIdHash: Uint8Array;
+            flags: Uint8Array;
+            signCount: Uint8Array;
+            aaguid: Uint8Array;
+            credentialIdLength: Uint8Array;
+            credentialId: Uint8Array;
+            credentialPublicKey: Uint8Array;
+            publicKey: {
+                "1": 2;
+                "3": -7;
+                "-1": 1;
+                "-2": Uint8Array;
+                "-3": Uint8Array;
+            };
+        };
     };
-    user: {
-        id: Uint8Array;
-        name: string;
-        displayName: string;
+    type: 'public-key';
+};
+export type CredentialAssertionData = {
+    authenticatorAttachment: "cross-platform";
+    id: string;
+    rawId: Uint8Array;
+    response: {
+        authenticatorData: {
+            rpIdHash: Uint8Array;
+            flags: Uint8Array;
+            signCount: Uint8Array;
+        };
+        clientDataJSON: Uint8Array;
+        signature: Uint8Array;
+        userHandle: Uint8Array | null;
     };
-    pubKeyCredParams: [{
-        alg: -7;
-        type: 'public-key';
-    }];
-    authenticatorSelection: {
-        authenticatorAttachment: 'cross-platform';
-        userVerification: 'required';
-        requireResidentKey: boolean;
-    };
-    timeout: number;
-    attestation: 'direct';
-    attestatinFormats: ['packed'];
+    type: 'public-key';
+    signatureBase: Uint8Array;
 };
 export declare function isPublicKeyCredentialSupported(): boolean;
-export declare function createPublicKeyCredential(options: publicKeyCredentialCreationOptions): Promise<globalThis.Credential>;
+export declare function createPublicKeyCredential(options: PublicKeyCredentialCreationOptions): Promise<Credential>;
+export declare function getAttestation(options: PublicKeyCredentialRequestOptions): Promise<globalThis.Credential>;
+export declare function getCredentialRegistrationData(credential: PublicKeyCredential): CredentialRegistrationData;
+export declare function getCredentialAssertionData(assertion: PublicKeyCredential): CredentialAssertionData;
+export declare function COSEtoASN1PublicKey(x: Uint8Array, y: Uint8Array): Uint8Array;
+export declare function verifyECP256Signature(signature: Uint8Array, signatureBase: Uint8Array, ASN1PublicKey: Uint8Array): boolean;
 export declare function ASN1ECP256PublicKeyByteArrayToPEMString(pkBuffer: Uint8Array): string;
 export declare function stringToByteArray(str: string): Uint8Array;
 export declare function byteArrayToHexString(uint8array: Uint8Array, space?: boolean): string;
